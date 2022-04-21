5212da8bc559d1b4a9258828c6135af268c614b2
Maura Kummer <maurkummer@yahoo.com>
2022-04-21 18:04:59 +0200

    excercise using the find()method

---
+++ findElementsInArray/app2.js
@@ -0,0 +1,5 @@
+//Use the find() method to return elements in a array that satisfies the provided testing function
+const numbers = [3, 52, 1, 89, 2];
+const found = numbers.find((element) => element > 10);
+
+console.log(found);
